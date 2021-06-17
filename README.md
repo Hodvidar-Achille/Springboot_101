fa4b70a5369ecadb11e23ed444fe4d109ee9d9d8
Hodvidar-Achille <Hodvidar@protonmail.com>
2021-06-17 15:16:45 +0200

    Add exercice 102

---
+++ README.md
@@ -0,0 +1,1 @@
+Springboot learning project using the book "Introduction to Java Spring Boot" https://g.co/kgs/PYNjrV
--- src/main/java/com/hodvidar/sb/controller/HomeController.java
+++ src/main/java/com/hodvidar/sb/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.hodvidar.sb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
     @RequestMapping("/")
     public String homePage() {
         return "index";
+    }
+
+    @RequestMapping("/variable")
+    public String homePage(Model model) {
+        model.addAttribute("myvar", "Exercise SP 102 says hello");
+        model.addAttribute("myvar2", "Hodvidar says hello too");
+        return "variableTemplate";
     }
 
 
