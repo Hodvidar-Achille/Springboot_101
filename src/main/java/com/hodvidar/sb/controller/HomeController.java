9eafa20a268777ae4cf1889be964984511821e73
Hodvidar-Achille <Hodvidar@protonmail.com>
2021-06-27 17:13:42 +0200

    Add SP exo 203 and 204

--- src/main/java/com/hodvidar/sb/controller/HomeController.java
+++ src/main/java/com/hodvidar/sb/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.hodvidar.sb.controller;
 
 import com.hodvidar.sb.model.Song;
+import com.hodvidar.sb.model.TvShow;
 import com.hodvidar.sb.utils.EmailAddressChecker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -57,6 +61,23 @@ public class HomeController {
                                final Model model) {
         model.addAttribute("song", song);
         return "confirmSong";
+    }
+
+    @GetMapping("/tvform")
+    public String loadTvForm(final Model model) {
+        model.addAttribute("tvshow", new TvShow());
+        return "tvForm";
+    }
+
+    @RequestMapping("/tvform")
+    public String loadTvForm(@Valid final TvShow tvShow,
+                             final Model model,
+                             final BindingResult result) {
+        model.addAttribute("tvshow", tvShow);
+        if(result.hasErrors()) {
+            return "tvForm";
+        }
+        return "tvShowConfirm";
     }
 
 
